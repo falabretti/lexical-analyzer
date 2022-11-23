7fc8e8b06b4c313b06c95558cbe4594b25a7cca4
Lucas Falabretti <lucasfalabretti7@gmail.com>
2022-11-23 18:25:37 -0300

    Add a history on InputSection

--- src/components/InputSection.tsx
+++ src/components/InputSection.tsx
@@ -1,51 +1,86 @@
-import { Box, Divider, TextField, Typography, useTheme } from "@mui/material";
+import { Box, Divider, Grid, List, ListItem, ListItemIcon, ListItemText, TextField, Typography, useTheme } from "@mui/material";
 import React, { useState } from "react";
 import { useContext } from "react";
 import { AutomatonContext, AutomatonContextType } from "../context/AutomatonContext";
+import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
+import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
+
+type TokenHistory = {
+  token: string,
+  valid: boolean
+}
 
 function InputSection() {
 
+  const { automaton, inputSymbol, resetAutomaton } = useContext(AutomatonContext) as AutomatonContextType;
   const theme = useTheme();
   const [value, setValue] = useState<string>('');
-  const { inputSymbol, resetAutomaton } = useContext(AutomatonContext) as AutomatonContextType;
+  const [history, setHistory] = useState<TokenHistory[]>([]);
 
   function handleInputToken(token: string) {
     // TODO case insensitive
     // TODO handle copy paste
     // TODO handle cursor position
     // TODO handle backspace
-    console.log(token);
+    // TODO handle fast typing
     inputSymbol(token.slice(-1));
   }
 
   function handleKeyPress(event: React.KeyboardEvent<HTMLInputElement>) {
     event.preventDefault();
-    handleInputToken(value);
+
     if (event.key === "Enter" || event.key === " ") {
+      setHistory([{ token: value, valid: automaton.isValid() }, ...history])
       setValue("");
       resetAutomaton();
+      return;
     }
+
+    handleInputToken(value);
   }
 
   return (
     <section>
       <Typography variant="h5" gutterBottom>Recognize Tokens</Typography>
       <Divider />
-      <Box sx={{ p: 2, pl: 0 }}>
-        <TextField
-          label="Token"
-          variant="outlined"
-          value={value}
-          onKeyUp={handleKeyPress}
-          sx={{
-            width: 0.35,
-            [theme.breakpoints.down("md")]: {
-              width: 1
-            }
-          }}
-          onChange={(event) => setValue(event.target.value.trimEnd())}
-        />
-      </Box>
+      <Grid container>
+        <Grid item xs={12} md={6}>
+          <Box sx={{ p: 2, pl: 0 }}>
+            <TextField
+              label="Token"
+              variant="outlined"
+              value={value}
+              onKeyUp={handleKeyPress}
+              sx={{
+                width: 0.71,
+                [theme.breakpoints.down("md")]: {
+                  width: 1
+                }
+              }}
+              onChange={(event) => setValue(event.target.value.trimEnd())}
+            />
+          </Box>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Box sx={{ p: 1 }}>
+            <Typography variant="h6">History</Typography>
+            <List>
+              {history.map((entry, id) => (
+                <ListItem key={id}>
+                  <ListItemIcon>
+                    {
+                      entry.valid
+                        ? <CheckCircleOutlineIcon color="success" />
+                        : <ErrorOutlineIcon color="error" />
+                    }
+                  </ListItemIcon>
+                  <ListItemText primary={entry.token} />
+                </ListItem>
+              ))}
+            </List>
+          </Box>
+        </Grid>
+      </Grid>
     </section>
   );
 }
